093c61186b3afd8cca2a945cfc6e763c2869d37a
mb0484 <mb0484@student.uni-lj.si>
2020-12-03 17:40:28 +0100

    Added age field to user entity.

--- models/src/main/java/com/webflix/users/models/entities/UserEntity.java
+++ models/src/main/java/com/webflix/users/models/entities/UserEntity.java
@@ -20,6 +20,9 @@ public class UserEntity {
 	@Column(name = "user_name")
 	private String user_name;
 
+	@Column(name = "user_age")
+	private Integer user_age;
+
 	public Integer getId() {
 		return id;
 	}
@@ -34,5 +37,13 @@ public class UserEntity {
 
 	public void setUser_name(String user_name) {
 		this.user_name = user_name;
+	}
+
+	public Integer getUser_age() {
+		return user_age;
+	}
+
+	public void setUser_age(Integer user_age) {
+		this.user_age = user_age;
 	}
 }
--- models/src/main/resources/sql-scripts/init.sql
+++ models/src/main/resources/sql-scripts/init.sql
@@ -1,1 +1,4 @@
-INSERT INTO users_data (user_name) VALUES ('Martin Bozic');
+INSERT INTO users_data (user_name, user_age) VALUES ('Martin Bozic', 23);
+INSERT INTO users_data (user_name, user_age) VALUES ('Anamarija Bozic', 18);
+INSERT INTO users_data (user_name, user_age) VALUES ('Rok Cej', 23);
+INSERT INTO users_data (user_name, user_age) VALUES ('Kaja Cej', 20);
